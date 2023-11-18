0dd1d398a853e4117091959bbba2be8acf129673
hashir <75959275+zzsleepyzzz@users.noreply.github.com>
2023-11-17 22:58:01 -0500

    added pip installation option

--- ascii_img_stuff/ascii.py
+++ ascii_img_stuff/ascii.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# hashirkz
+# https://github.com/hashirkz/ascii_stuff
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -89,26 +89,8 @@ def pretty_repr(img: np.ndarray, show=True, save=False, savepath: str=None) -> s
 
     return repr
 
-def app():
-    wd = os.getcwd()
-    img_path = input('path to img: ')
-    h = int(input('height for .txt file: '))
-    inv = input('invert y/n: ').strip() != 'n'
-    if not os.path.exists(img_path): 
-        print(f'unable to read {img_path} from {wd}')
-        return
-    
-    img = read_img(img_path, inv=inv)
-    img = rescale(img, h=h)
-    ascii = img_to_ascii(img)
-    savepath = f'{os.path.basename(img_path).split(".")[0]}.txt'
-    ascii = pretty_repr(ascii, show=False, save=True, savepath=savepath)
-    print(ascii)
-
-    print(f'saved to: {savepath}')
-
 if __name__ == '__main__':
-    app()
+    pass
 
     
 
--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ sudo apt update
 # sudo apt install python3.8
 pip3 install -r 'requirements.txt'
 
-sudo cp ascii.py /usr/bin/ascii
+sudo cp ./ascii_img_stuff/ascii.py /usr/bin/ascii
