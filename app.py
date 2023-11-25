8c00b7e2be8e6f1c2c26a9cffc107df306c533b7
hashir <75959275+zzsleepyzzz@users.noreply.github.com>
2023-11-24 21:36:35 -0500

    added more cli options

--- app.py
+++ app.py
@@ -6,17 +6,30 @@ from ascii import *
 def app():
     wd = os.getcwd()
     img_path = input('path to img: ')
-    h = int(input('height for .txt file: '))
+    h = input('height for .txt file: ')
+    try:
+        h = int(h)
+    except ValueError:
+        print(f'h: {h} invalid value for height')
+        return 1
+    
     inv = input('invert y/n: ').strip() != 'n'
+    save = input('save y/n: ').strip() != 'n'
+    chset = input('? character_set\nDEFAULT:  .:-=+*#%@\nOTHERS: ■█▓▒░: ')
+    
+    # default chset / character ramp
+    # other character ramps
+    if not chset: chset=' .:-=+*#%@'
+
     if not os.path.exists(img_path): 
         print(f'unable to read {img_path} from {wd}')
         return
     
     img = read_img(img_path, inv=inv)
     img = rescale(img, h=h)
-    ascii = img_to_ascii(img)
+    ascii = img_to_ascii(img, chset=chset)
     savepath = f'{os.path.basename(img_path).split(".")[0]}.txt'
-    ascii = pretty_repr(ascii, show=False, save=True, savepath=savepath)
+    ascii = pretty_repr(ascii, show=False, save=save, savepath=savepath)
     print(ascii)
 
     print(f'saved to: {savepath}')
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 sudo apt update
-# sudo apt install python3.8
+sudo apt install python3.8
 pip3 install -r 'requirements.txt'
 
 sudo cp ./app.py $HOME/.local/bin/ascii
